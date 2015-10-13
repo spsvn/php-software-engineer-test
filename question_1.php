347532730606fe8b72960820a33021422d187971
Kevin <wh00527@gmail.com>
2015-10-13 23:00:01 +1100

    finish test commit

--- question_1.php
+++ question_1.php
@@ -1,28 +1,83 @@
 <?php
 namespace SoftwareEngineerTest;
+use PDO;
+/**
+ * function to simply check input
+ * to prevent sql injection
+ */
+function validateInput($str)
+{
+    $str = str_replace("and","",$str);
+    $str = str_replace("execute","",$str);
+    $str = str_replace("update","",$str);
+    $str = str_replace("count","",$str);
+    $str = str_replace("chr","",$str);
+    $str = str_replace("mid","",$str);
+    $str = str_replace("master","",$str);
+    $str = str_replace("truncate","",$str);
+    $str = str_replace("char","",$str);
+    $str = str_replace("declare","",$str);
+    $str = str_replace("select","",$str);
+    $str = str_replace("create","",$str);
+    $str = str_replace("delete","",$str);
+    $str = str_replace("insert","",$str);
+    $str = str_replace("'","",$str);
+    $str = str_replace(" ","",$str);
+    $str = str_replace("or","",$str);
+    $str = str_replace("=","",$str);
+    $str = str_replace("%20","",$str);
+   return $str;
+}
 
 // Question 1a
-
 $DB_HOST = 'localhost';
 $DB_NAME = 'test';
 $DB_USER = 'test';
 $DB_PASS = 'test';
 
-// write your sql to get customer_data here
-$sql = "...";
+// get PDO DB
+try{
+    $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'test', 'test', array(PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
 
+//check whether get parameter or not
+if(isset($_GET['occupation_name'])){
+    $parameter = validateInput($_GET['occupation_name']);
+}else{
+    $parameter = null;
+}
+if( $parameter ){
+    $where  = '`occupation`.occupation_name =' ."'".$parameter."'";
+}else{
+    $where = '1';
+}
+$sql = sprintf('SELECT * FROM `customer` as `customer` left join `customer_occupation` as `occupation` on
+                    `customer`.customer_occupation_id = `occupation`.customer_occupation_id where %s',$where);
+$dbResult = $db->query($sql);
+$results = $dbResult->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
+
 
 <h2>Customer List</h2>
 
 <table>
-	<tr>
-		<th>Customer ID</th>
-		<th>First Name</th>
-		<th>Last Name</th>
-		<th>Occupation</th>
-	</tr>
-
-	<!-- Write your code here -->
-
+    <tr>
+        <th>Customer ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Occupation</th>
+    </tr>
+    <?php foreach($results as $result):?>
+         <tr>
+             <td><?php echo $result['customer_id']?></td>
+             <td><?php echo $result['first_name']?></td>
+             <td><?php echo $result['last_name']?></td>
+             <td><?php echo ( !is_null($result['occupation_name']) ) ? $result['occupation_name'] : 'un-employed';?></td>
+         </tr>
+    <?php endforeach;?>
 </table>
+
--- question_2.php
+++ question_2.php
@@ -7,17 +7,154 @@ namespace SoftwareEngineerTest;
  * Class Customer
  */
 abstract class Customer {
-	protected $id;
-	protected $balance = 0;
+    protected $id;
+    protected $balance = 0;
 
-	public function __construct($id) {
-		$this->id = $id;
-	}
+    public function __construct($id) {
+        $this->id = $id;
+    }
 
-	public function get_balance() {
-		return $this->balance;
-	}
+    public function get_balance() {
+        return $this->balance;
+    }
 }
 
 
-// Write your code below
+// question 2
+/**
+ * Class of Bronze Customer
+ */
+class Bronze_Customer extends Customer{
+    protected $type = 'Bronze';
+    /*
+     * get customer balance
+     * */
+    protected function getCustomerBalance(){
+        $balance = parent::get_balance();
+        return $balance;
+    }
+
+    /*
+     * make deposit
+     * */
+    public function deposit($value){
+        if($this->type == 'Bronze'){
+            $finalBalance = (float)$this->getCustomerBalance() + (float)$value;
+        }
+        return $finalBalance;
+    }
+    /*
+     * question 4 generate username of Bronze type of customer
+     * 20 digits random generated
+     * */
+    protected function generate_username(){
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < 20; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return 'B'.$randomString;
+    }
+}
+
+/**
+ * Class of Silver Customer
+ */
+class Silver_Customer extends Customer{
+    protected $type = 'Silver';
+    /*
+     * get customer balance
+     * */
+    protected function getCustomerBalance(){
+        $balance = parent::get_balance();
+        return $balance;
+    }
+    /*
+     * make deposit
+     * */
+    public function deposit($value){
+       if($this->type == 'Silver'){
+          $finalBalance = (float)$this->getCustomerBalance() + (float)$value * 0.05;
+        }
+        return $finalBalance;
+    }
+
+    /*
+     * question 4 generate username of Silver type of customer
+     * 20 digits random generated
+     * */
+    protected function generate_username(){
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < 20; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return 'S'.$randomString;
+    }
+}
+
+/**
+ * Class of Gold Customer
+ */
+class Gold_Customer extends Customer{
+    protected $type = 'Gold';
+    /*
+     * get customer balance
+     * */
+    protected function getCustomerBalance(){
+        $balance = parent::get_balance();
+        return $balance;
+    }
+
+    /*
+     * make deposit
+     * */
+    public function deposit($value){
+        if($this->type == 'Gold'){
+            $finalBalance = (float)$this->getCustomerBalance() + (float)$value * 0.1;
+        }
+        return $finalBalance;
+    }
+    /*
+     * question 4 generate username of Gold type of customer
+     * 20 digits random generated
+     * */
+    protected function generate_username(){
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < 20; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return 'G'.$randomString;
+    }
+}
+
+// question 3
+/*
+ * factory method of Customer
+ * based on give ID, then use factory method to initiate correct
+ * object
+ * */
+class CustomerFactory
+{
+    public static function get_instance($id)
+    {
+        $firstIdString = substr($id, 0, 1);
+        switch($firstIdString){
+            case 'B': return new Bronze_Customer($id);
+                    break;
+            case 'S': return new Silver_Customer($id);
+                break;
+            case 'G': return new Gold_Customer($id);
+                break;
+            default:
+                throw new Exception('InvalidArgument .');
+                break;
+        }
+    }
+}
+
+CustomerFactory::get_instance('G123456');
